c9bd2cd4bad0b0af242e43e9b5a45fa76d5df384
akshay <119416860+axai-kaizoku@users.noreply.github.com>
2024-09-06 16:53:10 +0530

    add forgot-password feature (#6)

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,13 +1,46 @@
 'use server';
 
-export async function RequestResetPass() {
-	const url =
-		'https://api.edify.club/edifybackend/v1/auth/request-password-reset';
-	const res = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
+type RequestOTPProps = {
+	message: string;
+	userId?: string;
+};
+
+export async function RequestOTP(email: string): Promise<RequestOTPProps> {
+	const response = await fetch(
+		'https://api.edify.club/edifybackend/v1/auth/request-password-reset',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email }),
 		},
-	});
-	return res;
+	);
+	return response.json();
+}
+
+type ResetPassProps = {
+	message: string;
+};
+
+export async function ResetPass(
+	userId: string,
+	otp: string,
+	password: string,
+): Promise<ResetPassProps> {
+	const response = await fetch(
+		'https://api.edify.club/edifybackend/v1/auth/verify-otp',
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				userId,
+				otp,
+				newPassword: password,
+			}),
+		},
+	);
+	return response.json();
 }
