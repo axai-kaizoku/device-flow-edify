e938c3806237ce89d6884ae9dfefd653685ad40f
Ashish Kumar Mishra <118009781+akm-engineer@users.noreply.github.com>
2024-09-09 20:11:29 +0530

    Redux setup (#7)
    Co-authored-by: Arman Mishra <arman2004562@gmail.com>

---
+++ src/app/store/authSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type UserData = {
+	id: string;
+	name: string;
+	email: string;
+};
+
+interface AuthState {
+	isAuthenticated: boolean;
+	userData: UserData | null;
+}
+
+const initialState: AuthState = {
+	isAuthenticated: false,
+	userData: null,
+};
+
+const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {
+		login(state, action: PayloadAction<{ userData: UserData }>) {
+			state.isAuthenticated = true;
+			state.userData = action.payload.userData;
+		},
+		logout(state) {
+			state.isAuthenticated = false;
+			state.userData = null;
+		},
+	},
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
