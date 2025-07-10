fe66bb768734bf80124fb00fdf650c1f783cbcc7
Akshay Yelle <02b3akshay@gmail.com>
2025-07-10 12:34:41 +0530

    update build

--- src/server/main/index.ts
+++ src/server/main/index.ts
@@ -1,5 +1,5 @@
 // const BASEURL = "https://gcp-api.edify.club";
-const BASEURL = "https://staging.deviceflow.ai";
-// const BASEURL = "https://9254aa94ebcc.ngrok-free.app";
+// const BASEURL = "https://staging.deviceflow.ai";
+const BASEURL = "https://bceee85acbd2.ngrok-free.app";
 
 export { BASEURL };
